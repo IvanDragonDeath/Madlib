e272464e631ce5999df150af5e28ed253bb7ddd9
kaydenforthman <forthmankayden@gmail.com>
2018-07-11 09:10:21 -0600

    Added styles

--- Madlibs/src/components/input.js
+++ Madlibs/src/components/input.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Input = () => {
+const Input = (title) => {
     return (
         <div className="input">
         <input />
-        <label>color</label>        
+        <label>{title}</label>        
        </div>
 
     )
